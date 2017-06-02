beda9c221e6a7e97309329835a96000c4227fcc8
lmk123 <me@limingkai.cn>
2017-06-02 15:12:09 +0800

    忘记删除 runtime 代码了

--- src/index.js
+++ src/index.js
@@ -4,15 +4,5 @@ import './assets/stylesheets/global.scss'
 
 import Vue from 'vue'
 import App from './app.vue'
-import { install, applyUpdate } from 'offline-plugin/runtime'
-
-install({
-  onUpdateReady () {
-    applyUpdate()
-  },
-  onUpdated () {
-    window.location.reload()
-  }
-})
 
 document.body.appendChild(new Vue(App).$mount().$el)
